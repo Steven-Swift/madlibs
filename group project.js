8f9e7b093a9c1eaed9d5c0966c88d54fa8f01e32
Steven Swift <84861311+Steven-Swift@users.noreply.github.com>
2021-08-12 15:43:29 -0400

    Add files via upload

---
+++ group project.js
@@ -0,0 +1,76 @@
+function adjectives()
+{
+	var a = document.querySelector("#adj1").value;
+	var b = document.querySelector("#adj2").value;
+	var c = document.querySelector("#adj3").value;
+	
+	document.querySelector("#result1").innerHTML =  a;
+	document.querySelector("#result2").innerHTML =  b;
+	document.querySelector("#result3").innerHTML =  c;
+}
+
+function nouns()
+{
+	var a = document.querySelector("#noun1").value;
+	var b = document.querySelector("#noun2").value;
+	var c = document.querySelector("#noun3").value;
+	
+	document.querySelector("#result4").innerHTML =  a;
+	document.querySelector("#result5").innerHTML =  b;
+	document.querySelector("#result6").innerHTML =  c;
+}
+
+function verbs()
+{
+	var a = document.querySelector("#verb1").value;
+	var b = document.querySelector("#verb2").value;
+	var c = document.querySelector("#verb3").value;
+	
+	document.querySelector("#result7").innerHTML =  a;
+	document.querySelector("#result8").innerHTML =  b;
+	document.querySelector("#result9").innerHTML =  c;
+}
+
+function story()
+{
+	var a = document.querySelector("#story1").value;
+	var b = document.querySelector("#story2").value;
+	var c = document.querySelector("#story3").value;
+	var d = document.querySelector("#story4").value;
+	var e = document.querySelector("#story5").value;
+	var f = document.querySelector("#story6").value;
+	var g = document.querySelector("#story7").value;
+	var h = document.querySelector("#story8").value;
+	var i = document.querySelector("#story9").value;
+	var j = document.querySelector("#story10").value;
+	var k = document.querySelector("#story11").value;
+	var l = document.querySelector("#story12").value;
+	var m = document.querySelector("#story13").value;
+	var n = document.querySelector("#story14").value;
+	var o = document.querySelector("#story15").value;
+	var p = document.querySelector("#story16").value;
+	var q = document.querySelector("#story17").value;
+	var r = document.querySelector("#story18").value;
+	var s = document.querySelector("#story19").value;
+	
+	
+	document.querySelector("#result10").innerHTML =  a;
+	document.querySelector("#result11").innerHTML =  b;
+	document.querySelector("#result12").innerHTML =  c;
+	document.querySelector("#result13").innerHTML =  d;
+	document.querySelector("#result14").innerHTML =  e;
+	document.querySelector("#result15").innerHTML =  f;
+	document.querySelector("#result16").innerHTML =  g;
+	document.querySelector("#result17").innerHTML =  h;
+	document.querySelector("#result18").innerHTML =  i;
+	document.querySelector("#result19").innerHTML =  j;
+	document.querySelector("#result20").innerHTML =  k;
+	document.querySelector("#result21").innerHTML =  l;
+	document.querySelector("#result22").innerHTML =  m;
+	document.querySelector("#result23").innerHTML =  n;
+	document.querySelector("#result24").innerHTML =  o;
+	document.querySelector("#result25").innerHTML =  p;
+	document.querySelector("#result26").innerHTML =  q;
+	document.querySelector("#result27").innerHTML =  r;
+	document.querySelector("#result28").innerHTML =  s;
+}
